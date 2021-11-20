1b5ed8355467d1f77cf339f92b4dc9fda123ca61
eline.brader <eline.brader@nedap.com>
2021-11-20 21:57:05 +0100

    Input can now be written in Version_ files!!!

--- src/main/java/com/nedap/healthcare/eline/Calculator.java
+++ src/main/java/com/nedap/healthcare/eline/Calculator.java
@@ -1,23 +1,24 @@
 package com.nedap.healthcare.eline;
 
-import com.nedap.healthcare.eline.symbols.SymbolTable;
 import com.nedap.healthcare.eline.visitor.MyPerfectElineVisitor;
 import com.nedap.healthcare.eline.visitor.MyPerfectSymbolTableVisitor;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class Calculator {
+
+    private static InputStream getFile(final String filename) throws IOException {
+        File file = new File("src/main/java/com/nedap/healthcare/eline/examples/" + filename);
+        return new FileInputStream(file);
+    }
+
     public static void main(String[] args) throws Exception {
-        String inputFile = null;
-        if (args.length > 0) {
-            inputFile = args[0];
-        }
-        var is = System.in;
-        if (inputFile != null) {
-            is = new FileInputStream(inputFile);
-        }
+        var is = Calculator.getFile("Version_1");
         var lexer = new ElineLexer(CharStreams.fromStream(is)); // je kunt ook CharStreams.fromString("x = 2\n2*x\n") doen ofzo
         var tokenStream = new CommonTokenStream(lexer);
         var parser = new ElineParser(tokenStream);
